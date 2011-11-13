c4d7265a6de74b550bbe39c674feef455c036025
amySukumunu <shoujo_sm@hotmail.com>
2011-11-13 23:15:39 +0800

    part2

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Sesame::Application.routes.draw do
 	resources :users 
+	resources :sessions
 		
 	root :to => 'pages#home'
   	match '/contact', 	:to => 'pages#contact'
   	match '/about',   	:to => 'pages#about'
 	match '/signup',   	:to => 'users#new'
-    
+    match '/signin',   	:to => 'sessions#new'
+  	match '/signout',   	:to => 'sessions#destroy'
     
     
 end
