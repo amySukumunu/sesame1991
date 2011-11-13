dcd8aa0c29b7ca5bd2938e6167fd61cbd3bc4e56
amySukumunu <shoujo_sm@hotmail.com>
2011-11-13 21:35:25 +0800

    hello

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sesame::Application.config.secret_token = 'be3e0eef255761ab17f90abc459dc5f8433eae4a62bd3f24731136325b55fa84dfb57317c79456ed0783601e84eb60ebeb84d6de9055c77adaefe2118a891ef4'
